65a5481dc771141a00347a90960ae2c3fbccf024
Josh Ferrara <scjosh2@gmail.com>
2018-09-10 15:15:49 -0700

    Small changes.

--- setup/streampush.sh
+++ setup/streampush.sh
@@ -13,12 +13,13 @@ then
 
     APP_PORT=8000
     RTMP_PORT=1935
+    APP_SECRET=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 128 | head -n 1)
 
-    echo "Use default ports: HTTP:8000, RTMP:1935? (Y/n)"
+    echo -n "Use default ports: HTTP=8000, RTMP=1935? (Y/n): "
     read USE_DEFAULTS
     if [ "$USE_DEFAULTS" == "n" ]
     then
-        echo "HTTP Port: "
+        echo -n "HTTP Port (Default: 8000): "
         read APP_PORT
 
         if [ "$APP_PORT" == "" ]
@@ -26,7 +27,7 @@ then
             APP_PORT=8000
         fi
 
-        echo "RTMP Port: "
+        echo -n "RTMP Port (Default: 1935): "
         read RTMP_PORT
 
         if [ "$APP_PORT" == "" ]
@@ -35,10 +36,8 @@ then
         fi
     fi
 
-    APP_SECRET=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 128 | head -n 1)
-
     eval "echo \"$(cat docker-compose.yml.tplt)\" > docker-compose.yml"
-
+    echo "Streampush is configured. Run \`streampush.sh start\` to start Streampush."
 elif [ "$1" == "start" ]
 then
     # Run docker-compose up

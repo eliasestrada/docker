1436c6913f65c5f8df47ec82e362085e529089db
Josh Ferrara <scjosh2@gmail.com>
2018-09-11 17:08:44 -0700

    Initial work for SSL support

--- setup/streampush.sh
+++ setup/streampush.sh
@@ -6,12 +6,13 @@ docker-compose --version
 
 if [ "$1" == "config" ]
 then
-    echo "Streampush configuration"
+    echo "Streampush configuration:\n\t1) App ports\n\t2) Nginx config\n\t3) Done"
 
     APP_PORT=8000
     RTMP_PORT=1935
     APP_SECRET=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 128 | head -n 1)
 
+    echo "App ports"
     echo -n "Use default ports: HTTP=8000, RTMP=1935? (Y/n): "
     read USE_DEFAULTS
     if [ "$USE_DEFAULTS" == "n" ]
@@ -33,10 +34,49 @@ then
         fi
     fi
 
+    echo "Nginx config"
+    echo -n "Would you like to setup an nginx reverse proxy for streampush with SSL? This is recommended for publicly-accesible installs. (Y/n)"
+    read REV_PRXY
+    if [ "$REV_PRXY" != "n" ]
+    then
+        echo -n "Please enter the FQDN of your installation (ex: streampush.io): "
+        read FQDN
+
+        SP_NGINX=`cat <<EOL
+nginx:
+        image: jwilder/nginx-proxy
+        volumes:
+            - ./spdata/certs:/etc/nginx/certs:ro
+            - /etc/nginx/vhost.d/
+            - /usr/share/nginx/html
+            - /var/run/docker.sock:/tmp/docker.sock:ro
+        labels:
+            - "com.github.jrcs.letsencrypt_nginx_proxy_companion.nginx_proxy"
+        ports:
+            - "80:80"
+            - "443:443"
+    ssl:
+        image: jrcs/letsencrypt-nginx-proxy-companion
+        volumes:
+            - ./spdata/certs:/etc/nginx/certs:rw
+            - /var/run/docker.sock:/var/run/docker.sock:ro
+            - /etc/nginx/vhost.d/
+            - /usr/share/nginx/html
+        depends_on:
+            - nginx
+EOL
+`
+
+        SP_VHOST="- VIRTUAL_HOST=${FQDN}"
+        SP_LEHOST="- LETSENCRYPT_HOST=${FQDN}"
+        SP_LEEMAIL="- LETSENCRYPT_EMAIL=fakeemail@asdasdasdadsdasd.com"
+    fi
+
     cat > docker-compose.yml <<EOL
 version: '3'
 
 services:
+    ${SP_NGINX}
     db:
         image: redis
     app:
@@ -50,6 +90,11 @@ services:
             - "$APP_PORT:8000"
         environment:
             - DJANGO_SECRET=$APP_SECRET
+            $SP_VHOST
+            $SP_LEHOST
+            $SP_LEEMAIL
+        depends_on:
+            - ssl
     relay:
         image: streampush/relay
         volumes:
